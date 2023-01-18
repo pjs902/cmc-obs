79425324c12fddb242ec60cad6b5e090889d7010
Peter Smith <smith.peter.902@gmail.com>
2023-01-18 13:57:42 -0400

    some basic tests

--- tests/test_obs.py
+++ tests/test_obs.py
@@ -19,4 +19,68 @@ def kinematics_object(snapshot):
 
 
 def test_LOS(kinematics_object):
-    kinematics_object.LOS_dispersion(stars_per_bin=25)
+    bin_centers, sigmas, delta_sigmas, mean_mass = kinematics_object.LOS_dispersion()
+
+    assert all(sigmas > 0)
+    assert all(sigmas < 30)
+    assert all(delta_sigmas > 0)
+    assert mean_mass > 0
+    assert len(bin_centers) == len(sigmas)
+
+
+
+def test_HubblePM(kinematics_object):
+    (
+        bin_centers,
+        sigma_r,
+        delta_sigma_r,
+        sigma_t,
+        delta_sigma_t,
+        mean_mass,
+    ) = kinematics_object.hubble_PMs()
+
+    assert all(sigma_r > 0)
+    assert all(sigma_r < 30)
+    assert all(sigma_t > 0)
+    assert all(sigma_t < 30)
+    assert all(delta_sigma_r > 0)
+    assert all(delta_sigma_t > 0)
+    assert mean_mass > 0
+
+    assert len(bin_centers) == len(sigma_r)
+
+
+def test_GaiaPM(kinematics_object):
+    (
+        bin_centers,
+        sigma_r,
+        delta_sigma_r,
+        sigma_t,
+        delta_sigma_t,
+        mean_mass,
+    ) = kinematics_object.gaia_PMs()
+
+    assert all(sigma_r > 0)
+    assert all(sigma_r < 30)
+    assert all(sigma_t > 0)
+    assert all(sigma_t < 30)
+    assert all(delta_sigma_r > 0)
+    assert all(delta_sigma_t > 0)
+    assert mean_mass > 0
+
+    assert len(bin_centers) == len(sigma_r)
+
+
+def test_number_density(kinematics_object):
+
+    (
+        bin_centers,
+        number_density,
+        delta_number_density,
+    ) = kinematics_object.number_density()
+
+    assert all(number_density > 0)
+    assert all(delta_number_density > 0)
+    assert all(bin_centers > 0)
+
+    assert len(bin_centers) == len(number_density)
